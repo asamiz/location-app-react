9c86962dd557c019f9ce33623c0a154fc6e77bdb
Ahmed Sami <ahmed.sami.elsherif@gmail.com>
2020-10-21 00:53:17 +0200

    :sparkles: Add Home page to the app

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
+import { HomePage } from "../../pages";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>{"HELLO"}</h1>
-      </header>
+      <HomePage />
     </div>
   );
 }
