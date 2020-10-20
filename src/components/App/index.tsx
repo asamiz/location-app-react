d877992b0d8189cae3c178f92d42812d69f5d287
Ahmed Sami <ahmed.sami.elsherif@gmail.com>
2020-10-21 00:21:09 +0200

    :building_construction: Structure app files and folders

---
+++ src/components/App/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{"HELLO"}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
