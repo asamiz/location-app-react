05e5330dbb8f0f69a572e52e25ce42cbb05e96b7
Ahmed Sami <ahmed.sami.elsherif@gmail.com>
2020-10-21 14:13:59 +0200

    Updates

---
+++ precache-manifest.7116676d70c1d658dad4245c2b1cc29e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1509532e5566ecd80aeaf61e7cb623ee",
+    "url": "/location-app-react/index.html"
+  },
+  {
+    "revision": "3b89cd4725da4584c097",
+    "url": "/location-app-react/static/css/main.cfbe9b82.chunk.css"
+  },
+  {
+    "revision": "9f6d2b9c93d084750d9b",
+    "url": "/location-app-react/static/js/2.9824d8ee.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/location-app-react/static/js/2.9824d8ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b89cd4725da4584c097",
+    "url": "/location-app-react/static/js/main.b26b30fc.chunk.js"
+  },
+  {
+    "revision": "95aa83953ac8ec845992",
+    "url": "/location-app-react/static/js/runtime-main.02684826.js"
+  }
+]);
